a600f03f3492055c35dda1ab925181b84212e26e
sukrima-080 <suk.rima.080@gmail.com>
2026-07-24 19:15:01 +0545

    new page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -4,18 +4,28 @@ export default function Team() {
   const [members, setMembers] = useState([]);
 
   useEffect(() => {
-    const API = import.meta.env.VITE_API_URL;
+  //   const API = import.meta.env.VITE_API_URL;
 
-    fetch(`${API}/api/users`)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Failed to fetch members");
-        }
-        return res.json();
-      })
-      .then((data) => setMembers(data))
-      .catch((err) => console.error(err));
-  }, []);
+  //   fetch(`${API}/api/users`)
+  //     .then((res) => {
+  //       if (!res.ok) {
+  //         throw new Error("Failed to fetch members");
+  //       }
+  //       return res.json();
+  //     })
+  //     .then((data) => setMembers(data))
+  //     .catch((err) => console.error(err));
+  // }, []);
+  const API = import.meta.env.VITE_API_URL;
+
+console.log("API URL:", API);
+
+useEffect(() => {
+  fetch(`${API}/api/users`)
+    .then((res) => res.json())
+    .then((data) => setMembers(data))
+    .catch(console.error);
+}, []);
 
   return (
     <div className="px-8 py-24">
